a55f27fb44923c8f194d37f6c8620e867e45a3d3
Mauricio Jost <mauricio.jost@gmail.com>
2016-02-29 18:50:27 +0100

    Provide support for old and new Hadoop API

--- src/main/scala/eu/pepot/eu/spark/inputsplitter/SplitWriter.scala
+++ src/main/scala/eu/pepot/eu/spark/inputsplitter/SplitWriter.scala
@@ -1,10 +1,11 @@
 package eu.pepot.eu.spark.inputsplitter
 
 import eu.pepot.eu.spark.inputsplitter.common._
-import org.apache.hadoop.mapreduce.{InputFormat, OutputFormat}
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.mapreduce
+import org.apache.hadoop.mapred
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.hadoop.fs.FileSystem
 import org.slf4j.LoggerFactory
 
 import scala.reflect.ClassTag
@@ -15,11 +16,24 @@ class SplitWriter(
 
   val logger = LoggerFactory.getLogger(this.getClass)
 
+  def selectiveSplitWriteOld[
+  K: ClassTag,
+  V: ClassTag,
+  I <: mapred.InputFormat[K, V] : ClassTag,
+  O <: mapred.OutputFormat[K, V] : ClassTag
+  ](
+    completeDirectory: String,
+    cutsDirectory: String
+  )(implicit sc: SparkContext): Unit = {
+    val cuttableRecords: RDD[(K, V)] = selectiveSplitRDDOld(completeDirectory)
+    cuttableRecords.saveAsHadoopFile[O](cutsDirectory)
+  }
+
   def selectiveSplitWrite[
   K: ClassTag,
   V: ClassTag,
-  I <: InputFormat[K, V] : ClassTag,
-  O <: OutputFormat[K, V] : ClassTag
+  I <: mapreduce.InputFormat[K, V] : ClassTag,
+  O <: mapreduce.OutputFormat[K, V] : ClassTag
   ](
     completeDirectory: String,
     cutsDirectory: String
@@ -28,9 +42,29 @@ class SplitWriter(
     cuttableRecords.saveAsNewAPIHadoopFile[O](cutsDirectory)
   }
 
+  def selectiveSplitRDDOld[
+  O <: mapred.OutputFormat[K, V] : ClassTag,
+  I <: mapred.InputFormat[K, V] : ClassTag,
+  V: ClassTag,
+  K: ClassTag
+  ](
+    completeDirectory: String
+  )(implicit sc: SparkContext): RDD[(K, V)] = {
+
+    implicit val fs = FileSystem.get(sc.hadoopConfiguration)
+
+    val files = FileLister.listFiles(completeDirectory)
+    val cuttableFiles = FilesMatcher.matches(files, condition)
+
+    logger.warn("Using input: {}", completeDirectory)
+    logger.warn("Detected splittables: {}", cuttableFiles)
+
+    sc.hadoopFile[K, V, I](cuttableFiles.toStringList())
+  }
+
   def selectiveSplitRDD[
-  O <: OutputFormat[K, V] : ClassTag,
-  I <: InputFormat[K, V] : ClassTag,
+  O <: mapreduce.OutputFormat[K, V] : ClassTag,
+  I <: mapreduce.InputFormat[K, V] : ClassTag,
   V: ClassTag,
   K: ClassTag
   ](
