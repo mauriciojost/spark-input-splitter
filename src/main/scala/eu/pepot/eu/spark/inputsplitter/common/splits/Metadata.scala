5b41ff15a8b651b7f50ce6e514940d6ee33e1739
Mauricio Jost <mauricio.jost@gmail.com>
2016-03-03 23:33:02 +0100

    Handle input and output streams in Metadata

--- src/main/scala/eu/pepot/eu/spark/inputsplitter/common/splits/Metadata.scala
+++ src/main/scala/eu/pepot/eu/spark/inputsplitter/common/splits/Metadata.scala
@@ -1,7 +1,7 @@
 package eu.pepot.eu.spark.inputsplitter.common.splits
 
 import eu.pepot.eu.spark.inputsplitter.common.file.{FileDetails, FileDetailsSet}
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{FSDataInputStream, FSDataOutputStream, FileSystem, Path}
 
 case class Metadata(
   splits: FileDetailsSet,
@@ -11,39 +11,55 @@ case class Metadata(
 
 object Metadata {
 
+  val BIG_KEY = "I"
+  val SMALL_KEY = "i"
+  val SPLIT_KEY = "s"
+  val ROW_SEPARATOR = ","
+  val LINE_SEPARATOR = "\n"
+
   val SPLITS_MAPPING_FILENAME = "splits.mapping"
 
   def load(splitsDirO: SplitsDir)(implicit fs: FileSystem): Metadata = {
     val splitsMappingFile = new Path(splitsDirO.getMetadataPath + "/" + SPLITS_MAPPING_FILENAME)
-    val fsDataInputStream = fs.open(splitsMappingFile)
-    val metadataContent = scala.io.Source.fromInputStream(fsDataInputStream).getLines().mkString("\n")
-    fsDataInputStream.close()
-    deserialize(metadataContent.getBytes())
-    // TODO FIX
+    var fsDataInputStream: FSDataInputStream = null
+    try {
+      fsDataInputStream = fs.open(splitsMappingFile)
+      val metadataContent = scala.io.Source.fromInputStream(fsDataInputStream).getLines().mkString("\n")
+      deserialize(metadataContent.getBytes())
+    } finally {
+      if (fsDataInputStream != null) {
+        fsDataInputStream.close()
+      }
+    }
   }
 
   def dump(md: Metadata, splitsDirO: SplitsDir)(implicit fs: FileSystem): Unit = {
     val splitsMappingFile = new Path(splitsDirO.getMetadataPath + "/" + SPLITS_MAPPING_FILENAME)
-    val fsDataOutputStream = fs.create(splitsMappingFile, true)
-    fsDataOutputStream.write(serialize(md))
-    fsDataOutputStream.close()
-    // TODO FIX
+    var fsDataOutputStream: FSDataOutputStream = null
+    try {
+      fsDataOutputStream = fs.create(splitsMappingFile, true)
+      fsDataOutputStream.write(serialize(md))
+    } finally {
+      if (fsDataOutputStream != null) {
+        fsDataOutputStream.close()
+      }
+    }
   }
 
   private def serialize(md: Metadata): Array[Byte] = {
-    val bigs = md.bigs.files.map(file => List("I", file.size, file.path.toString))
-    val smalls = md.smalls.files.map(file => List("i", file.size, file.path.toString))
-    val splits = md.splits.files.map(file => List("s", file.size, file.path.toString))
+    val bigs = md.bigs.files.map(file => List(BIG_KEY, file.size, file.path.toString))
+    val smalls = md.smalls.files.map(file => List(SMALL_KEY, file.size, file.path.toString))
+    val splits = md.splits.files.map(file => List(SPLIT_KEY, file.size, file.path.toString))
     val all = bigs ++ smalls ++ splits
-    all.map(_.mkString(",")).mkString("\n").getBytes
+    all.map(_.mkString(ROW_SEPARATOR)).mkString(LINE_SEPARATOR).getBytes
   }
 
   private def deserialize(md: Array[Byte]): Metadata = {
     val s = new String(md)
-    val lines = s.split("\n").map(line => line.split(","))
-    val bigs = lines.filter(_ (0) == "I").map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
-    val smalls = lines.filter(_ (0) == "i").map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
-    val splits = lines.filter(_ (0) == "s").map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
+    val lines = s.split(LINE_SEPARATOR).map(line => line.split(ROW_SEPARATOR))
+    val bigs = lines.filter(_ (0) == BIG_KEY).map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
+    val smalls = lines.filter(_ (0) == SMALL_KEY).map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
+    val splits = lines.filter(_ (0) == SPLIT_KEY).map(b => FileDetails(new Path(b(2)), b(1).toLong)).toList
     Metadata(FileDetailsSet(splits), FileDetailsSet(bigs), FileDetailsSet(smalls))
   }
 
