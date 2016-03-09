66c2528dd3f362d3cf10181293ea441186efa77d
Mauricio Jost <mauricio.jost@gmail.com>
2016-03-09 22:57:15 +0100

    Provide defaults for Condition

--- src/main/scala/eu/pepot/eu/spark/inputsplitter/common/file/matcher/Condition.scala
+++ src/main/scala/eu/pepot/eu/spark/inputsplitter/common/file/matcher/Condition.scala
@@ -1,7 +1,7 @@
 package eu.pepot.eu.spark.inputsplitter.common.file.matcher
 
 case class Condition(
-  biggerThan: Option[Int] = None,
+  biggerThan: Option[Int] = Some(1024 * 1024 * 128),
   namePattern: Option[String] = None,
   pathCondition: Option[String => Boolean] = None
 )
