d2b02380081432aeb4791ac9b8e6368988381b6c
Mauricio Jost <mauricio.jost@gmail.com>
2016-03-21 08:55:25 +0100

    Use .gz files in data-sample-00

--- xtras/scripts/data-sample-00/generate.py
+++ xtras/scripts/data-sample-00/generate.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 
 import random
+import subprocess as sub
 
 TOTAL_NRO_LINES=500000
 
-def write(name, nroLines): 
-    f = open("input/" + name + ".csv", "w" )
+def write(name, nroLines):
+    filename = "input/" + name + ".csv"
+    f = open(filename, "w" )
     for i in range(nroLines):
         line = str(random.randint(1, 700000)) + "," + str(random.randint(1,5000000)) 
         f.write(line + '\n')
     f.close()
 
+    p = sub.Popen(['gzip', filename], stdout=sub.PIPE, stderr=sub.PIPE)
+    output, errors = p.communicate()
+    print output
+
+
 print('*** Ensure the directory \'input\' exists...')
 
-write('big1', 45000000)
-write('big2', 20000000)
 write('small1', 50000)
 write('small2', 20000)
 write('small3', 30000)
 write('small4', 30000)
 write('small5', 30000)
+write('big1', 45000000)
+write('big2', 12000000)
+
 
 print('*** Done')
 
